88810bb5265e2a425d9a155e71f3928dcd782b0f
langald <silva-@list.ru>
2018-07-22 01:39:21 +0600

    Added social links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,17 +4,25 @@
         <div class="row">
             <ul class="col-12 footer-menu" >
                 <li class="footer-menu__item">
-                    <router-link :to="{name: 'home'}" >Home</router-link>
+                    <a href="mailto:larisaangald@gmail.com" target="_blank">
+                       <i class="far fa-envelope"></i>
+                    </a>
                 </li>
                 <li class="footer-menu__item">
-                    <router-link :to="{name: 'about'}" >About</router-link>
+                    <a href="https://t.me/Larissa_Angald" target="_blank">
+                       <i class="fab fa-telegram-plane"></i>
+                    </a>
+                </li>                
+                <li class="footer-menu__item">
+                    <a href="https://github.com/langald" target="_blank">
+                        <i class="fab fa-github"></i>
+                    </a>
                 </li>
                 <li class="footer-menu__item">
-                    <router-link :to="{name: 'about'}" >About1</router-link>
-                </li>
-                <li class="footer-menu__item">
-                    <router-link :to="{name: 'about'}" >About2</router-link>
-                </li>
+                    <a href="https://www.facebook.com/people/%D0%9B%D0%B0%D1%80%D0%B8%D1%81%D0%B0-%D0%90%D0%BD%D0%B3%D0%B0%D0%BB%D1%8C%D0%B4/100001912967674" target="_blank">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                </li>                
             </ul>           
         </div>
     </div>
@@ -56,5 +64,11 @@ export default {
     font-weight: bold;
     color: $gray-color;
     text-decoration: none;    
+}
+.footer-menu__item i {
+    font-size: 3rem;
+}
+.footer-menu__item i:hover {
+    color: $gray-light-color;
 }
 </style>
