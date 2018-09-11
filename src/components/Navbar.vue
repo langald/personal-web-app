8e1cde49a4838375b344b2ad6ea9baeee4478650
langald <silva-@list.ru>
2018-09-11 23:47:30 +0600

    added navbar toggle methods

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,17 +3,19 @@
         <div  class="col-12 d-md-none navbar-toggler-wrap">
             <button type="button" class="navbar-toggler" @click.prevent="toggleMenu()">
                 <span class="navbar-toggler-icon"></span>
-            </button>
+            </button>            
         </div>
       
-        <ul class="col-12 header-menu" >
+        <ul class="col-12 header-menu" v-show="headerMenuShownBtn">
             <li class="header-menu__item"
                 v-for="(item, index) in items"
                 :key="index"
+                @click="toggleMenu()"
                 >
                 <router-link :to="{name: item.to, params: {locale: $i18n.locale}}" >{{item.title}}</router-link>
             </li> 
-            <li class="header-menu__item">            
+            <li class="header-menu__item"
+                @click="toggleMenu()">            
                 <locale-switcher />            
             </li>               
         </ul>     
@@ -33,29 +35,21 @@ export default {
   },
   data() {
     return {
-      headerMenuShown: false
+      headerMenuShownBtn: window.innerWidth > 720      
     }
   },
-  computed: {
-    isWideScreen() {
-      console.log(0)
-      return window.innerWidth > 720
-    }
+  mounted () {
+    window.addEventListener('resize', this.handleResize);
   },
-  watch: {
-    isWideScreen: function (newQuestion, oldQuestion) {
-      /*
-      this.answer = 'Ожидаю, когда вы закончите печатать...'
-      this.debouncedGetAnswer()
-      */
-      console.log(11)
-    }
-
-  },
+  destroyed () {
+    window.removeEventListener('resize', this.handleResize);
+  },    
   methods: {
     toggleMenu() {      
-      this.headerMenuShown = !this.headerMenuShown
-      console.log(this.headerMenuShown)
+      this.headerMenuShownBtn = !this.headerMenuShownBtn      
+    },
+    handleResize() {      
+      this.headerMenuShownBtn = window.innerWidth > 720     
     }
   } 
 }
@@ -63,6 +57,26 @@ export default {
 
 <style lang="scss" scoped>
 @import "@/assets/styles/settings.scss";
+
+.navbar-toggler-wrap {
+    text-align: right;
+}
+.navbar-toggler {
+    padding: 30px 15px;   
+    line-height: 1;
+    background-color: transparent;
+    border: 1px solid transparent;
+    border-radius: .25rem;
+}
+.navbar-toggler-icon {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    vertical-align: middle;   
+    background: no-repeat 50%;
+    background-size: 100% 100%;  
+    background-image:url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 255, 255, 0.5)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E")
+}
 
 .header-menu {
     margin: 0;
@@ -102,33 +116,9 @@ export default {
     opacity: 1;   
 }
 
-
-
 @media only screen and (max-width: 720px) {
     .header-menu__item {
         display: block;       
     }
 }
-
-.navbar-toggler-wrap {
-    text-align: right;
-}
-.navbar-toggler {
-    padding: 30px 15px;   
-    line-height: 1;
-    background-color: transparent;
-    border: 1px solid transparent;
-    border-radius: .25rem;
-}
-.navbar-toggler-icon {
-    display: inline-block;
-    width: 30px;
-    height: 30px;
-    vertical-align: middle;   
-    background: no-repeat 50%;
-    background-size: 100% 100%;    
-
-    background-image:url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 255, 255, 0.5)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E")
-}
-
 </style>
